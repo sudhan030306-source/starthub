2da3d765d78294bbc8e0363e5a0bdb183ea8a428
sudhan030306-source <sudhan030306@gmail.com>
2026-04-19 08:56:23 +0530

    Delete freelance-site directory

--- freelance-site/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add any image domains here if you use next/image with external URLs
-  images: {
-    domains: [],
-  },
-};
-
-module.exports = nextConfig;
